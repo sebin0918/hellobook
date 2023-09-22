cebcbf360b2d196c6722a82750149719903b9369
Im Sebin <dlavkffk0918@gmail.com>
2023-09-22 15:54:42 +0900

    Initial commit

---
+++ src/main/java/com/springboot/hellobook/HellobookApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.hellobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellobookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/hellobook/HellobookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.hellobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
